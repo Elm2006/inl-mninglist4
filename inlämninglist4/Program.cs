edaac29d7747888ec6a6020e6c4f80f516bfd426
Knugen <isel06002@utb.vaxjo.se>
2024-02-02 13:11:44 +0100

    Add project files.

---
+++ inlämninglist4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+
+namespace inlämninglist4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ord = new string[] { "hej", "hej", "ja" };
+            char tecken = 'j';
+            Console.WriteLine(FinnsTecknetIAllaSträngar(ord, tecken));
+        }
+        static bool FinnsTecknetIAllaSträngar(string[]ord,char tecken)
+        {
+            int x = 0;
+            int y= 0;
+            for (int i = 0; i < ord.Length; i++)
+            {
+                y++;
+                if (ord[i].Contains(tecken))
+                {
+                    x++;
+                }
+            }
+            if (x ==y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
